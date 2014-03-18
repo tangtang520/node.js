af2ae7a3ef40980388b9329597466bf9a5d7d33e
tangtang520 <1315803594@qq.com>
2014-03-18 14:49:41 +0800

    node

---
+++ node.js
@@ -0,0 +1,48 @@
+/**
+ * Created by a11 on 14-3-17.
+ */
+var http=require('http');
+var cheerio=require('cheerio');
+var host='www.chapm25.com';
+var path='/city/beijing.html';
+var options = {
+    host:host,
+    port:80,
+    path:path,
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
+    }
+};
+var html = "";
+http.get(options,function (res) {
+    res.on('data', function (data) {
+        html += data;
+    });
+    res.on('end', function () {
+//        console.log(html)
+      var $=cheerio.load(html);
+       $('.container-fluid').find('.span4').each(function(i,ele){
+           var title=$(this).find('.statitlediv').find('.staname').text();
+           if(title=="所有监测站列表"){
+               $(this).find('tbody').find('tr').each(function(i,ele){
+                   var name=$(this).find('td').eq(0).find('a').text();
+				   
+				   if(name=='美国大使馆' || name=="农展馆" ||name=="奥体中心"||name=="通州"||name=="天坛")
+				 {
+	                   console.log(name)
+				   	
+				   }
+				   	
+					
+				   
+               
+			   
+			   
+			   })
+           }
+       });
+
+    });
+}).on('error', function (e) {
+        console.log(e);
+    });
